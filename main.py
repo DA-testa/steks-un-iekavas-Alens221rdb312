337dddfb4c39dca15f64141d8c3886671db86954
Alens221rdb312 <125511344+Alens221rdb312@users.noreply.github.com>
2023-02-19 20:41:07 +0000

    test1

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text, 1):
+    for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i))
             # Process opening bracket, write your code here
 
         if next in ")]}":
             if not opening_brackets_stack:
-                return i
+                return i + 1
             top = opening_brackets_stack.pop()
             if not are_matching(top.char, next):
-                return i
+                return i + 1
         # Process closing bracket, write your code here
         if opening_brackets_stack:
-            top = opening_brackets_stack.pop()
-            return top.position
+            top = opening_brackets_stack[0]
+            return top.position + 1
 
         return "Success"
 
@@ -34,18 +34,21 @@ def find_mismatch(text):
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    print(mismatch)
+    if isinstance(mismatch, int):
+        print(mismatch)
+    else:
+        print("Success")
     # Printing answer, write your code here
 
 
 if __name__ == "__main__":
     bruh = input()
-    if bruh == "I":
-        text = input()
+    if bruh[0] == "I":
         main()
-    else:
-        with open("test.txt") as f:
-            for line in f:
-                text = line.strip()
-                mismatch = find_mismatch(text)
-                print(mismatch)
+    elif bruh[0] == "F":
+        text = input()
+        mismatch = find_mismatch(text)
+        if isinstance(mismatch, int):
+            print(mismatch)
+        else:
+            print("Success")
