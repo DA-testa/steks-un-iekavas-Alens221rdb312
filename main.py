12ba7979e8a60a8f42f2775ba7298220d9374cb4
Alens221rdb312 <125511344+Alens221rdb312@users.noreply.github.com>
2023-02-19 20:54:25 +0000

    test3

--- main.py
+++ main.py
@@ -24,24 +24,24 @@ def find_mismatch(text):
             if not are_matching(top.char, next):
                 return i + 1
         # Process closing bracket, write your code here
-        if opening_brackets_stack:
-            top = opening_brackets_stack[0]
-            return top.position + 1
-
+    if opening_brackets_stack:
+        top = opening_brackets_stack[0]
+        return top.position + 1
+    else:
         return "Success"
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    if isinstance(mismatch, int):
-        print(mismatch)
-    else:
-        print("Success")
+    bruh = input()
+    if bruh[0] == "I":
+        text = input()
+        mismatch = find_mismatch(text)
+        if isinstance(mismatch, int):
+            print(mismatch)
+        else:
+            print("Success")
     # Printing answer, write your code here
 
 
 if __name__ == "__main__":
-    bruh = input()
-    if bruh[0] == "I":
-        main()
+    main()
